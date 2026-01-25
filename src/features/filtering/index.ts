ddfb23735b637eda4eba76027e348c5bd520d69e
오수진 <osujin@osujin-ui-MacBookPro.local>
2026-01-25 19:04:10 +0900

    fix: index.ts

--- src/features/filtering/index.ts
+++ src/features/filtering/index.ts
@@ -1,3 +1,3 @@
-export { CategoryList } from './ui/client/CategoryList'
 export { useCategoryList } from './model/useCategoryList'
 export { getCategories } from './model/getCategories'
+export { CategoryList } from './ui/client/CategoryList'
