58a882c30dac34dcf9fdb684d88136e0a063e68e
오수진 <osujin@osujin-ui-MacBookPro.local>
2026-02-12 11:06:47 +0900

    feat: room name setting

--- src/widgets/room-category-select/ui/RoomCategorySelect.tsx
+++ src/widgets/room-category-select/ui/RoomCategorySelect.tsx
@@ -13,7 +13,7 @@ const HEADER_CLASSNAME = 'mt-5'
 const TITLE_CLASSNAME = 'mr-auto ml-10 text-base font-semibold'
 const DESCRIPTION_CLASSNAME = 'mr-auto ml-10 text-sm'
 const LIST_WRAPPER_CLASSNAME =
-  'bg-secondary mx-4 mt-4 flex max-h-[68vh] max-w-350 flex-col items-center justify-center overflow-hidden rounded-2xl p-4'
+  'bg-secondary mx-4 mt-4 flex max-h-[70vh] min-w-87.5 flex-col items-center justify-center overflow-hidden rounded-2xl p-4 overflow-x-auto'
 
 export function RoomCategorySelect({
   accessToken,
